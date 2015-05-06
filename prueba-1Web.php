dfc4be75a187619518b569a5f17e6227066617da
cjcm15 <cjcm15@yahoo.com.ar>
2015-05-06 20:01:17 -0300

    6

---
+++ prueba-1Web.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html>
+<head lang="es">
+    <meta charset="UTF-8">
+    <title></title>
+    <link rel="stylesheet" href="CSS/estilos.css"/>
+</head>
+<body>
+100  1OOOO
+
+<?php
+/**
+ * Created by PhpStorm.
+ * User: cmi5
+ * Date: 06/05/2015
+ * Time: 11:42
+ */
+echo 'casa. <br/><br/>';
+
+
+$perro = 'Lobo';
+
+echo $perro;
+
+?>
+
+
+<div class="caja"><?php echo $perro; ?></div>
+
+
+</body>
+</html>
